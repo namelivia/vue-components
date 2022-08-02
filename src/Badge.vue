74d4c2e96320f419fcf83119d31c9799c0559b54
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2022-08-02 10:48:45 +0200

    Add Badge component

---
+++ src/Badge.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+span(class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800")
+  | {{text}}
+</template>
+<script lang="js">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: "Badge",
+  props: {
+    text: String,
+  },
+})
+</script>
