12ecf26d761e79ee32f623ddb82afc25f2562d82
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2023-02-23 11:21:16 +0100

    Avatar on Navbar

--- src/RightContent.vue
+++ src/RightContent.vue
@@ -2,15 +2,11 @@
 div(
   class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
 )
-  span(class="bg-gray-800 p-1 text-gray-400")
-    | {{ $i18n.locale }}
-  span(class="bg-gray-800 p-1 text-gray-400")
-    | {{ currentUserEmail }}
-  span(class="bg-gray-800 p-1 text-gray-400")
-    | {{ currentUserName }}
   img(
+    class="rounded-full p-1"
+    style="height: 50px;"
     :src="currentUserPicture"
-    :alt="currentUserName"
+    :alt="avatarAlt"
   )
 </template>
 <script lang="js">
@@ -29,6 +25,11 @@ export default defineComponent({
     },
     currentUserPicture: {
       type: String,
+    },
+  },
+  computed: {
+    avatarAlt: function () {
+      return this.currentUserName + ` (${this.currentUserEmail})`
     },
   },
 })
