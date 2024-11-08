70210b335c3c8a905d4ab4e833a6cb99cdc33aad
José Ignacio Amelivia Santiago <hello@namelivia.com>
2024-11-08 20:15:27 +0100

    Write basic tests

--- src/TextInput.cy.js
+++ src/TextInput.cy.js
@@ -1,8 +1,37 @@
 import TextInput from './TextInput.vue'
 
 describe('<TextInput />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-vue
-    cy.mount(TextInput)
+
+  it('should properly render a label', () => {
+      cy.mount(TextInput, {
+        props: {
+          label: 'Name'
+        }
+      })
+      cy.get('label').should('have.text', 'Name')
+  })
+  it('should properly render a placeholder', () => {
+      cy.mount(TextInput, {
+        props: {
+          placeholder: 'Enter your name'
+        }
+      })
+      cy.get('input').should('have.attr', 'placeholder', 'Enter your name')
+  })
+  it('should properly render a value', () => {
+      cy.mount(TextInput, {
+        props: {
+          text: 'John Doe'
+        }
+      })
+      cy.get('input').should('have.value', 'John Doe')
+  })
+  it('should disallow input when disabled', () => {
+      cy.mount(TextInput, {
+        props: {
+          disabled: true
+        }
+      })
+      cy.get('input').should('be.disabled')
   })
 })
