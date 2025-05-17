6179caea2b4c018c4f2383ce56f4c1b54d9644d7
José Ignacio Amelivia Santiago <hello@namelivia.com>
2025-05-17 17:34:40 +0200

    Initialize storybook

---
+++ src/Buttons/ResetButton/ResetButton.stories.js
@@ -0,0 +1,13 @@
+import ResetButton from './ResetButton.vue';
+
+export default {
+  title: 'Buttons/ResetButton',
+  component: ResetButton,
+  tags: ['autodocs'],
+}
+
+export const Default = {
+  args: {
+    text: 'Reset Button',
+  },
+}
---
+++ src/Buttons/SecondaryButton/SecondaryButton.stories.js
@@ -0,0 +1,13 @@
+import SecondaryButton from './SecondaryButton.vue';
+
+export default {
+  title: 'Buttons/SecondaryButton',
+  component: SecondaryButton,
+  tags: ['autodocs'],
+};
+
+export const Default = {
+  args: {
+    text: 'Secondary Button',
+  },
+};
