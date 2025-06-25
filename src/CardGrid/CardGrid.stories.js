c0d6aa898277fa0f9a3877127be8e8785bfd264d
José Ignacio Amelivia Santiago <hello@namelivia.com>
2025-06-25 17:24:42 +0200

    Update CardGrid stories

--- src/CardGrid/CardGrid.stories.js
+++ src/CardGrid/CardGrid.stories.js
@@ -1,4 +1,5 @@
 import CardGrid from './CardGrid.vue';
+import Card from '../Card/Card.vue';
 
 export default {
   title: 'CardGrid',
@@ -6,6 +7,20 @@ export default {
   tags: ['autodocs'],
 }
 export const Default = {
-  args: {
-  },
+  render: () => ({
+    components: { CardGrid, Card },
+    template: `
+      <CardGrid>
+          <Card title="Card Title" image="https://picsum.photos/640/480">
+            <p>This is the one card.</p>
+          </Card>
+          <Card title="Card Title 2" image="https://picsum.photos/640/480?2">
+            <p>This is the second card.</p>
+           </Card>
+           <Card title="Card Title 3" image="https://picsum.photos/640/480?3">
+             <p>This is the third card.</p>
+           </Card>
+       </CardGrid>
+    `,
+  }),
 };
