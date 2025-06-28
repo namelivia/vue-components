c2fd0672034f54184ad64d6d568aab6519235a10
José Ignacio Amelivia Santiago <hello@namelivia.com>
2025-06-28 16:53:14 +0200

    Themes experiment

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,30 @@
-/** @type { import('@storybook/vue3-vite').Preview } */
 import '../styles/index.css';
-const preview = {
-  parameters: {
-    controls: {
-      matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: 'one',
+    toolbar: {
+      icon: 'paintbrush',
+      items: [
+        { value: 'one', title: 'One Theme' },
+        { value: 'another', title: 'Another Theme' },
+      ],
     },
   },
 };
 
-export default preview;
+export const decorators = [
+  (story, context) => {
+    const theme = context.globals.theme || 'one';
+
+    return {
+      components: { story },
+      template: `<div :class="theme + '-theme'"><story /></div>`,
+      setup() {
+        return { theme };
+      },
+    };
+  },
+];
